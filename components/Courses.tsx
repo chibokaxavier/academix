6800f00093c8a5f7c68b5bd1c935f31c8e8324e8
manlikexavy <chibokaxavier@gmail.com>
2023-07-29 12:35:45 -0700

    .

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -8,6 +8,11 @@ import React, {
 import courses from "@/courses";
 
 const Courses = () => {
+  const [isPythonActive, setIsPythonActive] = useState(false);
+
+  // const handleButtonClick = () => {
+  //   setIsActive(!isActive);
+  // };
   const python = courses.filter((course) => {
     return course.type === "python";
   });
@@ -15,8 +20,6 @@ const Courses = () => {
   const webdev = courses.filter((course) => {
     return course.type === "Webdev";
   });
-  console.log(webdev);
-  
 
   const [dataArray, setDataArray] = useState([python]);
 
@@ -29,20 +32,36 @@ const Courses = () => {
           published every month
         </p>
         <div className="flex flex-col lg:flex-row   lg:space-x-6 font-semibold cursor-pointer pb-5">
-          <p className="lg:hover:underline hover:text-gray-500">Python</p>
-          <p onClick={() => setDataArray([webdev])}> Excel</p>
-          <p> Web Development</p>
-          <p>Javascript</p>
-          <p>Data Science</p>
-          <p>Amazon AWS</p>
+          <p
+            onClick={() => setDataArray([python])}
+            className="lg:hover:underline hover:text-gray-500"
+          >
+            Python
+          </p>
+          <p className="lg:hover:underline hover:text-gray-500"> Excel</p>
+          <p
+            className="lg:hover:underline hover:text-gray-500"
+            onClick={() => setDataArray([webdev])}
+          >
+            {" "}
+            Web Development
+          </p>
+          <p className="lg:hover:underline hover:text-gray-500">Javascript</p>
+          <p className="lg:hover:underline hover:text-gray-500">Data Science</p>
+          <p className="lg:hover:underline hover:text-gray-500">Amazon AWS</p>
         </div>
       </div>
 
-      <div className="w-full h-[300px] hidden lg:block bg-red border-2">
+      <div className="w-[50%] h-fit my-5 hidden lg:block bg-red border-2">
         {dataArray.map((data) => (
           <div>
-            {data.map((python) => (
-              <div>{python.name}</div>
+            {data.map((course) => (
+              <div className="p-2 space-y-5 flex flex-col cursor-pointer">
+                <p className="text-2xl font-bold">{course.header}</p>
+                <p className="text-base">{course.text}</p>
+                <img src={course.image} alt="" />
+                <p className="font-bold text-sm">{course.name}</p>
+              </div>
             ))}
           </div>
         ))}
--- courses.ts
+++ courses.ts
@@ -1,7 +1,8 @@
 const courses = [
   {
     id: 1,
-    text: "Expand your career opportunities with Python Take one of Udemy’s range of Python courses and learn how to code using this incredibly useful language. Its simple syntax and readability makes Python perfect for Flask, Django, data science, and machine learning. You’ll learn how to build everything from games to sites to apps. Choose from a range of courses that will appeal to",
+    header: "Expand your career opportunities with Python ",
+    text: "Take one of Udemy’s range of Python courses and learn how to code using this incredibly useful language. Its simple syntax and readability makes Python perfect for Flask, Django, data science, and machine learning. You’ll learn how to build everything from games to sites to apps. Choose from a range of courses that will appeal to....",
     type: "python",
     name: "Machine Learning A-Z™: AI, Python & R + ChatGPT Bonus [2023]",
     description:
@@ -12,7 +13,8 @@ const courses = [
   },
   {
     id: 2,
-    text:"Build websites and applications with Web Development The world of web development is as wide as the internet itself. Much of our social and vocational lives play out on the internet, which prompts new industries aimed at creating, managing, and debugging the websites and applications that we increasingly rely on.",
+    header: "Build websites and applications with Web Development",
+    text: " The world of web development is as wide as the internet itself. Much of our social and vocational lives play out on the internet, which prompts new industries aimed at creating, managing, and debugging the websites and applications that we increasingly rely on.",
     type: "Webdev",
     name: "The Complete 2023 Web Development Bootcamp",
     description:
@@ -20,16 +22,6 @@ const courses = [
     image: "/webdev.avif",
     instructor: "John Samson",
     number: `485,105 `,
-  },
-  {
-    id: 3,
-    type: "python",
-    name: "Machine Learning A-Z™: AI, Python & R + ChatGPT Bonus [2023]",
-    description:
-      "Learn to create Machine Learning Algorithms in Python and R from two Data Science experts. Code templates included.",
-    image: "/machinelanguage.jpg",
-    instructor: "James Balek",
-    number: `965,105 `,
   },
 ];
 export default courses;
